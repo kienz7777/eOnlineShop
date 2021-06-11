410a82939a0fc68828618964262550a4d98c1986
Otis.Vu <otis.vu@nexcel.vn>
2021-06-11 08:52:21 +0700

    first commit

---
+++ eOnlineShop.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eOnlineShop.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
